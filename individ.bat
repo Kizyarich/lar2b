cf6a17ea4976d0df0e0b2de95f67cdd667c2cdf6
Kizyarich <iw.sabin@yandex.ru>
2024-05-20 15:56:49 +0300

    first commit

---
+++ individ.bat
@@ -0,0 +1,20 @@
+echo off
+md Sabin
+cd Sabin
+md Sabin
+md Ivan
+pause
+ecgo>27102003.txt
+cd Ivan
+ecgo>5.txt
+pause
+cd ..
+cd ..
+del Sabin /S /Q /F
+pause
+cd Sabin
+rd Ivan
+rd Sabin
+cd ..
+rd Sabin
+pause
